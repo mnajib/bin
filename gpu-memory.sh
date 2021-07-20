dd24860baa15bd5531dcab1098931fd199f9b352
Najib <mnajib@gmail.com>
2021-07-20 15:40:29 +0800

    some more commond to get info of gpu memory

--- gpu-memory.sh
+++ gpu-memory.sh
@@ -1,3 +1,12 @@
 #!/run/current-system/sw/bin/env bash
 
+LC_ALL=C lspci -v | grep -EA10 "3D|VGA" | grep 'prefetchable'
+
+#echo "--------------------"
+#sudo dmesg | grep drm
+
+#echo "--------------------"
+#sudo dmesg | grem VRAM
+
+echo "--------------------"
 glxinfo | egrep -i 'device|memory'
