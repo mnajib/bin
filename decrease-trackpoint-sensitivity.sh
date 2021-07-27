faf295636a0cceb8ed173d91bc7a17aee7ff453f
Najib <mnajib@gmail.com>
2021-07-28 03:18:22 +0800

    Increase a little trackpoint sensitivity; older config to lower

--- decrease-trackpoint-sensitivity.sh
+++ decrease-trackpoint-sensitivity.sh
@@ -4,6 +4,9 @@
 #xinput list-props 15 | grep -i coordinate
 #	Coordinate Transformation Matrix (198):	0.500000, 0.000000, 0.000000, 0.000000, 0.500000, 0.000000, 0.000000, 0.000000, 1.000000
 
-xinput set-prop 15 198 0.5, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 1.0
+xinput set-prop 15 198 0.7, 0.0, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 1.0
 #xinput list-props 15 | grep -i coordinate
 #	Coordinate Transformation Matrix (198):	0.500000, 0.000000, 0.000000, 0.000000, 0.500000, 0.000000, 0.000000, 0.000000, 1.000000
+
+# Disable acceleration
+#xset m 1 0
