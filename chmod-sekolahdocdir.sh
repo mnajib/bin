d45658abb869fc9068a829c0a547a72105bbd581
najib@mahirah <mnajib@gmail.com>
2021-08-12 07:13:33 +0800

    Change chmod-sekolahdocdic.sh to source directory

--- chmod-sekolahdocdir.sh
+++ chmod-sekolahdocdir.sh
@@ -1,7 +1,8 @@
 #!/run/current-system/sw/bin/env bash
 
 #p=$1
-p="/mnt/sekolahdocdir"
+#p="/mnt/sekolahdocdir"
+p="/export/sekolahdocdir"
 
 find ${p} -type d -print0 | xargs -0 chmod 775
 find ${p} -type f -print0 | xargs -0 chmod 664
