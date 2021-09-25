21fb902e0900e55285f3456793acf7b59354391b
Juliani@keira <juliani.jaffar@gmail.com>
2021-09-25 12:22:09 +0800

    Switch between dual and single monitor for Keira

--- init-second-monitor-ThinkVisionForKeira.sh
+++ init-second-monitor-ThinkVisionForKeira.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-xrandr --output LVDS-1 --mode 1600x900 --primary --auto --output VGA-1 --mode 1280x1024 --right-of LVDS-1 --auto
+
+# set dual monitors
+dual () {
+    #xrandr --output VGA-0 --primary --left-of HDMI-0 --output HDMI-0 --auto
+    xrandr --output LVDS-1 --mode 1280x800 --primary --auto --output VGA-1 --mode 1280x1024 --left-of LVDS-1 --auto
+}
+
+# set single monitor
+single () {
+    #xrandr --output HDMI-0 --off
+    xrandr --output VGA-1 --off
+}
