6bc9ba503f56f9de1574547541a2bd23066dd5be
Najib@aisyah <mnajib@gmail.com>
2021-09-25 11:46:48 +0800

    Add umask before create skeleton directories for tugasan-sekolah.

--- sekolah-tugasan-init.sh
+++ sekolah-tugasan-init.sh
@@ -10,7 +10,9 @@
 #mkdir "${1}/03 Hasil tugasan"
 #mkdir "${1}/04 Bukti handar hasil tugasan"
 
-umask 0022
+#umask 0022
+umask 0002
+
 mkdir "01 Tugasan"
 mkdir "02 Bukti daftar kehadiran"
 mkdir "03 Hasil tugasan"
