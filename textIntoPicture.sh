f83337a38b696c60ec284d905ea5b5cc26ce3b26
najib@mahirah <mnajib@gmail.com>
2021-09-27 14:40:46 +0800

    Add text into picture

--- textIntoPicture.sh
+++ textIntoPicture.sh
@@ -15,6 +15,17 @@ inputf=$2
 outputf="namestamped-${1}-${2}"
 
 #convert ${inputf} -fill ${textcolor} -gravity North -pointsize 24 -annotate +0+100 ${text} ${outputf}
-convert ${inputf} -fill ${textcolor} -gravity North -pointsize 24 -annotate +0+100 ${text} ${outputf}
+#convert ${inputf} -fill ${textcolor} -gravity North -pointsize 24 -annotate +0+100 ${text} ${outputf}
 #convert ${inputf} -font ${font} -background ${background} -fill ${textcolor} -gravity North -pointsize 30 -annotate +0+100 ${text} ${outputf}
 
+convert -verbose -density 150 ${inputf} -quality 100 -sharpen 0x1.0 -fill ${textcolor} -gravity North -pointsize 24 -annotate +0+100 ${text} ${outputf}
+#convert -verbose -density 150 input.pdf -quality 100 -sharpen 0x1.0 -fill blue -gravity North -pointsize 24 -annotate +0+100 "Muhammad Na\'im Bin Mohd Najib,\n1 Beta, Matematik,\nRabu 2021-08-25." output.pdf
+
+#convert           \
+#   -verbose       \
+#   -density 150   \
+#   -trim          \
+#    test.pdf      \
+#   -quality 100   \
+#   -flatten       \
+#   -sharpen 0x1.0 \
