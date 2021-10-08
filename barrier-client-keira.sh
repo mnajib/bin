f3bd005f6fa799b3126f3a7dcea658b96b3740de
Juliani@keira <juliani.jaffar@gmail.com>
2021-10-09 00:28:02 +0800

    barrierc for keira without SSL

--- barrier-client-keira.sh
+++ barrier-client-keira.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-barrierc --no-daemon --restart --name keira --enable-crypto 192.168.123.152
+#barrierc --no-daemon --restart --name keira --enable-crypto 192.168.123.152
+barrierc --no-daemon --restart --name keira 192.168.123.152
+
