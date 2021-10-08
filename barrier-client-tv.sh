2851487f7ff4a0c3588643996aac57bda3294a30
M Najib BI <mnajib@gmail.com>
2021-10-09 00:20:48 +0800

    barrierc without SSL for tv

--- barrier-client-tv.sh
+++ barrier-client-tv.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-barrierc --no-daemon --restart --name tv --enable-crypto 192.168.123.152
+#barrierc --no-daemon --restart --name tv --enable-crypto 192.168.123.152
+barrierc --no-daemon --restart --name tv 192.168.123.152
