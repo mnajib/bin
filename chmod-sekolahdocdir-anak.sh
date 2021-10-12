4bcd7fd7ea6fc6b6d85633b045ded84da17ac5ea
najib@mahirah <mnajib@gmail.com>
2021-10-12 17:28:37 +0800

    chmod-sekolahdocdic-anak

---
+++ chmod-sekolahdocdir-anak.sh
@@ -0,0 +1,25 @@
+#!/run/current-system/sw/bin/env bash
+
+#p=$1
+#p="/mnt/sekolahdocdir"
+p="/export/sekolahdocdir"
+dirs=( "03 Naqib" "04 Nur Nasuha" "05 Na'im" )
+
+#echo ${dirs[1]}
+#echo ${dirs[*]}
+#echo ${#dirs[@]}
+
+c=0
+while [ $c -le ${#dirs[@]} ]
+do
+	# chmod directories
+	find "${p}/${dirs[${counter}]}" -type d -print0 | xargs -0 sudo chmod -v 775
+
+	# chmod files
+	find "${p}/${dirs[${counter}]}" -type f -print0 | xargs -0 sudo chmod -v 664
+
+	((c++))
+done
+
+sync
+sync
--- chmod-sekolahdocdir.sh
+++
@@ -1,8 +0,0 @@
-#!/run/current-system/sw/bin/env bash
-
-#p=$1
-#p="/mnt/sekolahdocdir"
-p="/export/sekolahdocdir"
-
-find ${p} -type d -print0 | xargs -0 sudo chmod 775
-find ${p} -type f -print0 | xargs -0 sudo chmod 664
