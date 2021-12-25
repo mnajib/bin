ad346eee776258daccbc4f7a8931467d61070341
Najib Ibrahim <mnajib@gmail.com>
2021-12-26 00:25:55 +0800

    set keyboard layout

---
+++ toggleKeyboardLayout.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#(setxkbmap -query | grep -q "layout:\s\+us") && setxkbmap se || setxkbmap us
+
+#setxkbmap -query | grep -q 'variant:\s\+intl') && setxkbmap -layout us || setxkbmap -layout us -variant intl
+
