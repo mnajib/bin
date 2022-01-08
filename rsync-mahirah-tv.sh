38089b2cc13043688c786af7d75766f5837f2a77
Najib Ibrahim <mnajib@gmail.com>
2022-01-08 09:50:50 +0800

    Merge some rsync script into one.

--- rsync-mahirah-tv.sh
+++
@@ -1,9 +0,0 @@
- rsync -azP \
- --exclude /home/najib/bin \
- --exclude /home/najib/.xmonad \
- --exclude '/home/najib/.nix*' \
- --exclude /home/najib/.cache \
- --exclude /home/najib/.bash_history \
- --exclude /home/najib/.gitconfig \
- najib@mahirah:/home/najib \
- /home
