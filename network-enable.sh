5325c00fa9cdb16e4bde8e674014aa62f2d3e191
Najib <mnajib@gmail.com>
2022-02-03 06:21:12 +0800

    scrip to enable (wireless) network, tested on x230.

---
+++ network-enable.sh
@@ -0,0 +1,4 @@
+nmcli radio all on
+nmcli connection show
+nmcli connection up b996e2ba-0f0a-41d8-9d1a-2b32e55ceee8 -a
+#nmcli connection up b996e2ba-0f0a-41d8-9d1a-2b32e55ceee8 password Naqib123Nasuha456Naim789
