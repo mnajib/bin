8302ab10c6c5a9418c26452c8ba8726c83793840
Najib@delldesktop <mnajib@gmail.com>
2022-10-09 07:05:16 +0800

    script to start StarDewValley with xypher

---
+++ StarDewValley-run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#USER=naim
+PATH1=~/.wine/drive_c
+PATH2=${PATH1}/MyPrograms/Stardew.Valley.v1.5.4
+PATH3=${PATH1}/users/${USER}/Application\ Data/StardewValley
+PATH4=${PATH1}/users/${USER}/AppData/StardewValley
+PATH5=${PATH2}/Stardew\ Valley
+
+# Ready custom nested display
+# Thinkpad X220 sakinah: 1366x768
+#Xephyr -br -ac -noreset -screen 1080x768 :1 &
+#Xephyr -br -ac -noreset -screen 1280x960 :1 &
+#Xephyr -br -ac -noreset -screen 1366x740 :1 &  # sakinah
+Xephyr -br -ac -noreset -screen 1278x750 :1 &   # zahrah
+
+# Reset StardewValley custom resolution
+#cd ~/.wine/drive_c/users/nurnasuha/Application\ Data/StardewValley
+#cat ~/.wine/drive_c/MyPrograms/Stardew.Valley.v1.5.4/config/startup_preferences > startup_preferences
+
+#cd ~/.wine/drive_c/users/nurnasuha/AppData/StardewValley
+#cat ~/.wine/drive_c/MyPrograms/Stardew.Valley.v1.5.4/config/startup_preferences > startup_preferences
+cat ~/.wine/drive_c/MyPrograms/Stardew.Valley.v1.5.4/config/startup_preferences > ~/.wine/drive_c/users/${USER}/AppData/Roaming/StardewValley/startup_preferences
+
+# Start the game
+cd ~/.wine/drive_c/MyPrograms/Stardew.Valley.v1.5.4/Stardew\ Valley/
+#DISPLAY=:1 wine ./Stardew\ Valley.exe
+#DISPLAY=:1 wine ${PATH5}/Stardew\ Valley.exe
+#cd ${PATH5}
+DISPLAY=:1 wine ./Stardew\ Valley.exe
