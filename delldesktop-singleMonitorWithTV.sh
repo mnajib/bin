81be2b9708c970f22a005c60dce233919f16b82a
Najib@delldesktop <mnajib@gmail.com>
2022-10-09 23:15:02 +0800

    delldesktop-singleMonitorWithTV.sh

---
+++ delldesktop-singleMonitorWithTV.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Panasonic TV
+
+#cvt -r 1366 768
+#xrandr --newmode "1368x768_60.00"  85.86  1368 1440 1584 1800  768 769 772 795  -HSync +Vsync
+#xrandr -q
+#xrandr --addmode VGA-1 "1368x768R"
+#xrandr --output VGA-1 --mode "1368x768R"
+
+gtf 1366 768 60
+xrandr --newmode "1368x768_60.00"  85.86  1368 1440 1584 1800  768 769 772 795  -HSync +Vsync
+xrandr -q
+xrandr --addmode VGA-1 "1368x768_60.00"
+xrandr --output VGA-1 --mode "1368x768_60.00"
+
+# copied from arandr
+#xrandr \
+#	--output LVDS-1 --mode 1280x800 --pos 44x768 --rotate normal \
+#	--output VGA-1 --primary --mode 1368x768_60.00 --pos 0x0 --rotate normal \
+#	--output HDMI-1 --off \
+#	--output DP-1 --off \
+#	--output HDMI-2 --off \
+#	--output DP-2 --off \
+#	--output DP-3 --off
