f7c9073de700bb66d8733531404f8f3507a871c0
Najib Ibrahim <mnajib@gmail.com>
2022-10-09 14:24:43 +0800

    barrier for tv and khadijah

---
+++ barrierc-tv-to-barriers-khadijah.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#barrierc --no-daemon --restart --name tv --enable-crypto 192.168.123.152
+#barrierc --no-daemon --restart --name tv 192.168.123.152
+barrierc --no-daemon --disable-crypto --restart --name tv khadijah
