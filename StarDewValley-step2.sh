46f0722d867f9c89bda139f29c409803f64bb5c6
Nasuha <nasuha@sakinah.local>
2022-10-18 15:02:50 +0800

    StarDewValley start script

--- StarDewValley-step2.sh
+++ StarDewValley-step2.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
-#USER=nurnasuha
-#USER=naim
-
 PATH1=~/.wine/drive_c
 PATH2=${PATH1}/MyPrograms/Stardew.Valley.v1.5.4
-PATH3=${PATH1}/users/${USER}/Application\ Data/StardewValley
-PATH4=${PATH1}/users/${USER}/AppData/StardewValley
+PATH3=${PATH1}/users/nurnasuha/Application\ Data/StardewValley
+PATH4=${PATH1}/users/nurnasuha/AppData/StardewValley
 PATH5=${PATH2}/Stardew\ Valley
 
 # Ready custom nested display
 # Thinkpad X220 sakinah: 1366x768
 #Xephyr -br -ac -noreset -screen 1080x768 :1 &
 #Xephyr -br -ac -noreset -screen 1280x960 :1 &
-Xephyr -br -ac -noreset -screen 1366x740 :1 &  # sakinah
-#Xephyr -br -ac -noreset -screen 1278x750 :1 &   # zahrah
+#Xephyr -br -ac -noreset -screen 1366x740 :1 &
 
 # Reset StardewValley custom resolution
 #cd ~/.wine/drive_c/users/nurnasuha/Application\ Data/StardewValley
@@ -22,13 +18,11 @@ Xephyr -br -ac -noreset -screen 1366x740 :1 &  # sakinah
 
 #cd ~/.wine/drive_c/users/nurnasuha/AppData/StardewValley
 #cat ~/.wine/drive_c/MyPrograms/Stardew.Valley.v1.5.4/config/startup_preferences > startup_preferences
-cat ~/.wine/drive_c/MyPrograms/Stardew.Valley.v1.5.4/config/startup_preferences > ~/.wine/drive_c/users/${USER}/AppData/Roaming/StardewValley/startup_preferences
+#cat ${PATH2}/config/startup_preferences > ${PATH4}/startup_preferences
 
 # Start the game
 cd ~/.wine/drive_c/MyPrograms/Stardew.Valley.v1.5.4/Stardew\ Valley/
 #DISPLAY=:1 wine ./Stardew\ Valley.exe
 #DISPLAY=:1 wine ${PATH5}/Stardew\ Valley.exe
 #cd ${PATH5}
-#DISPLAY=:1 
-#wine Stardew\ Valley.exe
 DISPLAY=:1 wine ./Stardew\ Valley.exe
