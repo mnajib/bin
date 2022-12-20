7d3f8df5a61231f513c9535f1406633e94312fd6
Najib Ibrahim <mnajib@gmail.com>
2022-12-20 20:56:15 +0800

    Fix init-second-monitor for keira and zahrah

--- init-second-monitor1280x1024-forZahrah.sh
+++ init-second-monitor1280x1024-forZahrah.sh
@@ -6,11 +6,13 @@
 #   init-second-monitor1280x1024-forKeira.sh single
 #
 
+echo "-----------------> ${1} <-----------------"
+
 export DISPLAY=:0
 #export XAUTHORITY=/home/juliani/.Xauthority
 
-internal=LVDS-1 # right
-external=VGA-1  # left
+internal="LVDS-1" # right
+external="VGA-1"  # left
 
 #dmode="$(cat /sys/class/drm/card0-VGA-1/status)"
 
@@ -18,10 +20,8 @@ external=VGA-1  # left
 dual () {
     #xrandr --output VGA-0 --primary --left-of HDMI-0 --output HDMI-0 --auto
     #xrandr --output LVDS-1 --mode 1280x800 --primary --auto --output VGA-1 --mode 1280x1024 --left-of LVDS-1 --auto
-    #xrandr --output $internal --mode 1280x800 --primary --auto --output $external --mode 1280x1024 --left-of $internal --auto
-    #xrandr --output $internal --mode 1280x800 --primary --pos 1280x224 --output $external --mode 1280x1024 --pos 0x0
-    #xrandr --output $internal --mode 1280x800 --primary --pos 1280x480 --output $external --mode 1280x1024 --pos 0x0
-    xrandr --output $internal --mode 1440x900 --primary --pos 1440x480 --output $external --mode 1280x1024 --pos 0x0
+    #xrandr --output ${internal} --mode 1280x800 --primary --pos 1280x414 --output ${external} --mode 1280x1024 --pos 0x0 # zahrah
+    xrandr --output LVDS-1 --mode 1280x800 --primary --pos 1280x414 --output VGA-1 --mode 1280x1024 --pos 0x0 # zahrah
 }
 
 # set single monitor
