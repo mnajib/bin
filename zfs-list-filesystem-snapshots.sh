4873b867a8b60710728fb724dbd696f5a99e292f
Najib Ibrahim <mnajib@gmail.com>
2023-06-08 10:24:09 +0800

    zfs-list-filesystem-snapshots.sh default for all host

--- zfs-list-filesystem-snapshots.sh
+++ zfs-list-filesystem-snapshots.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-watch '\
+function theList {
+  watch '\
   echo "----------------------------------------------------------------------------------"; \
   echo " zfs list -t filesystem"; \
   echo "----------------------------------------------------------------------------------"; \
@@ -20,3 +21,65 @@ watch '\
   echo ""; \
   zfs list -t snapshot najibzfspool1/root; \
   '
+}
+
+function theUniversalList {
+  watch '\
+  echo "----------------------------------------------------------------------------------"; \
+  echo " zfs list -t filesystem"; \
+  echo "----------------------------------------------------------------------------------"; \
+  zfs list -t filesystem; \
+  echo ""; \
+  echo "----------------------------------------------------------------------------------"; \
+  echo " zfs list -t snapshot"; \
+  echo "----------------------------------------------------------------------------------"; \
+  echo ""; \
+  zfs list -t snapshot; \
+  '
+}
+
+function listFilesystems {
+  local a='\
+  echo "----------------------------------------------------------------------------------"; \
+  echo " zfs list -t filesystem"; \
+  echo "----------------------------------------------------------------------------------"; \
+  zfs list -t filesystem; \
+  '
+  echo $a
+}
+
+function listSnapshots {
+  local a='\
+  echo "----------------------------------------------------------------------------------"; \
+  echo " zfs list -t snapshot"; \
+  echo "----------------------------------------------------------------------------------"; \
+  echo ""; \
+  echo "najibzfspool1/home"; \
+  echo "------------------"; \
+  echo ""; \
+  zfs list -t snapshot najibzfspool1/home; \
+  echo ""; \
+  echo "najibzfspool1/root"; \
+  echo "------------------"; \
+  echo ""; \
+  zfs list -t snapshot najibzfspool1/root; \
+  '
+  echo $a
+}
+
+#theList
+#theUniversalList
+
+#listFilesystems
+#listSnapshots
+#watchCommand=`($listFilesystems)($listSnapshots)`
+#watch $watchCommand
+
+case "${1}" in
+  'customdesktop')
+     theList
+     ;;
+  *)
+     theUniversalList
+     ;;
+esac
