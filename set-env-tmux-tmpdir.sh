4d69965b541a29ce755cae35b5cf284f168fed6b
Najib Ibrahim <mnajib@gmail.com>
2023-09-01 07:44:54 +0800

    set env needed for run tmux over mosh

--- set-env-tmux-tmpdir.sh
+++ set-env-tmux-tmpdir.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-export TMUX_TMPDIR=$XDG_RUNTIME_DIR
+function printTheVars {
+  echo "XDG_RUNTIME_DIR: ${XDG_RUNTIME_DIR}"
+  echo "TMUX_TMPDIR: ${TMUX_TMPDIR}"
+}
+
+printTheVars
+export TMUX_TMPDIR="${XDG_RUNTIME_DIR}"
+printTheVars
