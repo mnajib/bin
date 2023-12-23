1b12ad06f58efa21d9bd61057e380b6022929fc4
Najib Ibrahim <mnajib@gmail.com>
2023-12-24 06:55:10 +0800

    rebuild font cache after rsync fonts dir

--- rsync-fonts.sh
+++ rsync-fonts.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 rsync -avz najib@khadijah:~/.fonts .
+#fc-list -v | grep -i edward
+ln -s ~/.fonts ~/.local/share/fonts
+rm -vRf ~/.cache/fontconfig
+fc-cache --really-force --verbose
+#fc-list -v | grep -i edward
