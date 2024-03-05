5180eebe20d80fa9b17dc8d15171af689c338726
Najib Ibrahim <mnajib@gmail.com>
2024-03-05 10:00:58 +0800

    workaround for sweethome3d exiting on start

--- sweethome3d-workaround.sh
+++ sweethome3d-workaround.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#
+# References:
+#   - https://github.com/NixOS/nixpkgs/issues/71752
+#
+
 export JAVA_TOOL_OPTIONS="-Dcom.eteks.sweethome3d.j3d.useOffScreen3DView=true"
 sweethome3d
+
+#MESA_GL_VERSION_OVERRIDE=3.0 sweethome3d
