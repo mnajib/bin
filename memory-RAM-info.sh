8bc6c7378119a64403ddedbd892b068dffbf950b
Najib Ibrahim <mnajib@gmail.com>
2024-09-23 09:45:31 +0800

    total the memory install in memory-RAM-info.sh

--- memory-RAM-info.sh
+++ memory-RAM-info.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-sudo dmidecode | awk '/^Memory Device$/ {main=1; print; next} main && /^[ \t]*Size: .*|^[ \t]*Bank Locator: .*|^[ \t]*Speed: .*/ {print}'
+#sudo dmidecode | awk '/^Memory Device$/ {main=1; print; next} main && /^[ \t]*Size: .*|^[ \t]*Bank Locator: .*|^[ \t]*Speed: .*/ {print}'
+
+# With totaled memory installed
+sudo dmidecode | awk '/^Memory Device$/ {main=1; print; next} main && /^[ \t]*Size: .*|^[ \t]*Bank Locator: .*|^[ \t]*Speed: .*/ {print; if ($3 == "GB") sum+=$2} END {print "Total Memory Installed:", sum, "GB"}'
