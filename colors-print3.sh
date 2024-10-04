336572f671bfd2b031fed1981881457a78ce5c11
Najib Ibrahim <mnajib@gmail.com>
2024-10-04 10:45:55 +0800

    add columns in colors-print3.sh

--- colors-print3.sh
+++ colors-print3.sh
@@ -11,16 +11,24 @@ print_colors() {
     printf "%-30s %-30s\n" "Standard Foreground Colors" "Bright Foreground Colors"
     echo "-----------------------------------------------------------"
 
-    fg1=30
-    fg2=90
+    fg1=30 # black foreground
+    fg2=90 # bright-black foreground
+    bg1=40 # black background
+    bg2=47 # white background
 
     # Loop through standard colors (0-7)
     for f in {0..7}; do
-        # Column-1: Print standard color
-        printf "\033[${fg1}m%-30s\033[0m" "Fg1: ${fg1}"  # Standard foreground color
+        # Column-1: Print standard foreground colors, with black background 
+        printf "\033[${fg1};${bg1}m%-30s\033[0m" "Fg1: ${fg1}"  # Standard foreground color
 
-        # Column-2: Print bright color
-        printf "\033[${fg2}m%-30s\033[0m" "Fg2: ${fg2}"  # Bright foreground color
+        # Column-2: Print bright foreground colors, with black background
+        printf "\033[${fg2};${bg1}m%-30s\033[0m" "Fg2: ${fg2}"  # Bright foreground color
+
+        # Column-1: Print standard foreground colors, with white background 
+        printf "\033[${fg1};${bg2}m%-30s\033[0m" "Fg1: ${fg1}"  # Standard foreground color
+
+        # Column-2: Print bright foreground colors, with white background
+        printf "\033[${fg2};${bg2}m%-30s\033[0m" "Fg2: ${fg2}"  # Bright foreground color
 
         ((fg1++))
         ((fg2++))
@@ -41,16 +49,24 @@ print_colors() {
     printf "%-30s %-30s\n" "Standard Backgrounds" "Bright Backgrounds"
     echo "-----------------------------------------------------------"
 
-    bg1=40
-    bg2=100
+    fg1=37  # white foreground
+    fg2=30  # black foreground
+    bg1=40  # black background
+    bg2=100 # bright-black background
 
     # Loop through standard colors (0-7)
     for f in {0..7}; do
         # Column-1: Print standard color
-        printf "\033[${bg1}m%-30s\033[0m" "Bg1: ${bg1}"  # Standard foreground color
+        printf "\033[${fg1};${bg1}m%-30s\033[0m" "Bg1: ${bg1}"  # Standard foreground color
 
         # Column-2: Print bright color
-        printf "\033[${bg2}m%-30s\033[0m" "Bg2: ${bg2}"  # Bright foreground color
+        printf "\033[${fg1};${bg2}m%-30s\033[0m" "Bg2: ${bg2}"  # Bright foreground color
+
+        # Column-1: Print standard color
+        printf "\033[${fg2};${bg1}m%-30s\033[0m" "Bg1: ${bg1}"  # Standard foreground color
+
+        # Column-2: Print bright color
+        printf "\033[${fg2};${bg2}m%-30s\033[0m" "Bg2: ${bg2}"  # Bright foreground color
 
         ((bg1++))
         ((bg2++))
