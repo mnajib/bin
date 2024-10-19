121703b56c480faedecb0f3f782244b8414f139c
Najib Ibrahim <mnajib@gmail.com>
2024-10-19 20:49:57 +0800

    color names for basic standard ANSI colors (16 colors)

--- colors-print4.sh
+++ colors-print4.sh
@@ -5,31 +5,31 @@
 #declare -A data2=(
 data=(
     # Standard colors ---------------------------------------------------------
-    "0,Black,#000000,30,40" # Standard black as black
-    "1,Red,#FF0000,31,41" # Bright red as red
-    "2,Green,#00FF00,32,42" # Bright green as green
-    "3,Yellow,#FFFF00,33,43" # Bright yellow as yellow
-    "4,Blue,#0000FF,34,44" # Bright blue as blue
-    "5,Magenta,#FF00FF,35,45" # Bright magenta as magenta
-    "6,Cyan,#00FFFF,36,46" # Bright cyan as cyan
+    "0,black,#000000,30,40" # Standard black as black
+    "1,red,#FF0000,31,41" # Bright red as red
+    "2,green,#00FF00,32,42" # Bright green as green
+    "3,yellow,#FFFF00,33,43" # Bright yellow as yellow
+    "4,blue,#0000FF,34,44" # Bright blue as blue
+    "5,magenta,#FF00FF,35,45" # Bright magenta as magenta
+    "6,cyan,#00FFFF,36,46" # Bright cyan as cyan
     #"7,White,#FFFFFF,37,47" # as White
-    "7,White,#D3D3D3,37,47" # Light gray (instead of pure white) as White
+    "7,white,#D3D3D3,37,47" # Light gray (instead of pure white) as White
     # Bright colors -----------------------------------------------------------
     #"8,Bright_Black,#808080,90,100"  # Bright Black
-    "8,Bright_Black,#555753,90,100"  # Dark gray (for contrast) as Bright Black
+    "8,brightblack,#555753,90,100"  # Dark gray (for contrast) as Bright Black
     #"9,Bright_Red,#FF5555,91,101"    # Bright Red
-    "9,Bright_Red,#EF2929,91,101"    # Lighter red (for brightness) as Bright Red
+    "9,brightred,#EF2929,91,101"    # Lighter red (for brightness) as Bright Red
     #"10,Bright_Green,#55FF55,92,102"  # as Bright Green
-    "10,Bright_Green,#8AE234,92,102"  # Lighter green (for brightness) as Bright Green
+    "10,brightgreen,#8AE234,92,102"  # Lighter green (for brightness) as Bright Green
     #"11,Bright_Yellow,#FFFF55,93,103"  # as Bright Yellow
-    "11,Bright_Yellow,#FCE94F,93,103"  # Lighter yellow (for brightness) as Bright Yellow
+    "11,brightyellow,#FCE94F,93,103"  # Lighter yellow (for brightness) as Bright Yellow
     #"12,Bright_Blue,#5555FF,94,104"   # as Bright Blue
-    "12,Bright_Blue,#32AFFF,94,104"   # Lighter blue (for brightness) as Bright Blue
+    "12,brightblue,#32AFFF,94,104"   # Lighter blue (for brightness) as Bright Blue
     #"13,Bright_Magenta,#FF55FF,95,105" # as Bright Magenta
-    "13,Bright_Magenta,#AD7FA8,95,105" # Lighter magenta (for brightness) as Bright Magenta
+    "13,brightmagenta,#AD7FA8,95,105" # Lighter magenta (for brightness) as Bright Magenta
     #"14,Bright_Cyan,#55FFFF,96,106"    # as Bright Cyan
-    "14,Bright_Cyan,#34E2E2,96,106"    # Lighter cyan (for brightness) as Bright Cyan
-    "15,Bright_White,#FFFFFF,97,107"   # Pure white (kept as is) as Bright White (same as standard white)
+    "14,brightcyan,#34E2E2,96,106"    # Lighter cyan (for brightness) as Bright Cyan
+    "15,brightwhite,#FFFFFF,97,107"   # Pure white (kept as is) as Bright White (same as standard white)
 )
 
 # Simulated 2D array data stored as strings
