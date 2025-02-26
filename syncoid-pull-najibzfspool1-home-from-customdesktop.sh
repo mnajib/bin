e7be64ab14810db528bf094bf63be5d2e293b668
Najib Bin Ibrahim <najib@gmail.com>
2025-02-26 09:28:19 +0800

    set readonly=on for MyTank/backups/offsite datasets

--- syncoid-pull-najibzfspool1-home-from-customdesktop.sh
+++ syncoid-pull-najibzfspool1-home-from-customdesktop.sh
@@ -5,4 +5,15 @@
 #   Done test run with success
 #
 
+# You should set readonly=on for MyTank/backups/offsite if:
+#   You want all datasets under MyTank/backups/offsite/ to be protected from accidental modifications.
+#   You want to prevent accidental file edits or deletions that could break your backup integrity.
+#   You use Syncoid for one-way sync, and you don't plan to manually change anything inside MyTank/backups/offsite.
+#
+# This will make all sub-datasets (MyTank/backups/offsite/*) read-only, unless overridden.
+# Future Syncoid syncs will still work because Syncoid uses zfs send and zfs receive, which bypass the read-only restriction.
+#sudo zfs set readonly=on MyTank/backups/offsite
+#
+#sudo zfs set readonly=on MyTank/backups/offsite/najibzfspool1
+
 sudo syncoid root@customdesktop.localdomain:najibzfspool1/home MyTank/backups/offsite/najibzfspool1
