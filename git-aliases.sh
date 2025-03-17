291f75265fa14e091fb7dcb7be2c1cf6251d0f93
= <muhammad.naim.bin.mohd.najib@gmail.com>
2025-03-17 08:03:28 +0800

    git-aliases.sh: change from using bash aliases to git aliases

--- git-aliases.sh
+++ git-aliases.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-alias git-hist="git log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cd)' --graph --date=short --all"
+#alias git-hist="git log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cd)' --graph --date=short --all"
+
+#git log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cr)'
+#git log --pretty=oneline --decorate --graph
+#git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
+
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+#git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cr)" --graph --date=short'
+git config --global alias.hist 'log --pretty=format:"%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cd)" --graph --date=short --all'
+git config --global alias.type 'cat-file -t'
+git config --global alias.dump 'cat-file -p'
+
+#git branch -a -vv
+git config --global alias.branchall 'branch -a -vv'
