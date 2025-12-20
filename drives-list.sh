8d7a864248edd14f5a14fb699b2ebd134463d394
Najib Ibrahim <mnajib@gmail.com>
2025-12-20 09:14:02 +0800

    drives-list.sh: include by-uuid

--- drives-list.sh
+++ drives-list.sh
@@ -2,4 +2,6 @@
 
 ls -l /dev/disk/by-id/ata-* | grep -v -- -part | awk '{print $11 " -> " $9}' | sort
 echo ""
+ls -l /dev/disk/by-uuid/* | grep -v -- -part | awk '{print $11 " -> " $9}' | sort
+echo ""
 lsscsi -g
