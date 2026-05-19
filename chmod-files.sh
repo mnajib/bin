bcf6d3e55babc5dcdf8e3b0bd8b5044f8baa9fcd
Muhammad Naqib <m.naqib.bin.m.najib@gmail.com>
2026-05-19 19:24:02 +0800

    update chmod-dirs.sh and chmod-files.sh

--- chmod-files.sh
+++ chmod-files.sh
@@ -1,6 +1,7 @@
 #!/run/current-system/sw/bin/env bash
 
-p=$1
+#p=$1
 
 #find /mnt/sekolahdocdir -type f -print0 | xargs -0 chmod 664
-find $1 -type f -print0 | xargs -0 chmod 664
+#find $1 -type f -print0 | xargs -0 chmod 664
+find "${1}" -type f -print0 | xargs -0 chmod -v "${2}"
